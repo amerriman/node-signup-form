bb6f7869250d66355ec14cb935b069e498b898a1
amerriman <amm434@gmail.com>
2015-08-24 16:32:36 -0600

    complete

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('<form method="post" action="/submit"><input name="email" type="email" required><input type="submit"></form>');
+});
+
+//no 'next' needed?
+router.post('/submit', function(req, res) {
+  res.redirect('/success');
+});
+
+router.get('/success', function(req, res){
+  res.send("Success!");
 });
 
 module.exports = router;
