7f2c1bd70b5b165f05f01797bac8ba04cd9dded6
amerriman <amm434@gmail.com>
2015-08-24 21:01:15 -0600

    small edit

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+//This works with, and without "next"...so what's the difference?
 router.get('/', function(req, res, next) {
   res.send('<form method="post" action="/submit"><input name="email" type="email" required><input type="submit"></form>');
 });
